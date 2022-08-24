096d830f99037e26fcaf8806e2d6bb5c13d59b2b
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-08-24 15:45:03 -0400

    written js file

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -12,3 +12,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Select password length
+  var passwordLength = prompt("Choose the password length between 8-128.");
+  while (passwordLength < 8 || passwordLength > 128)
+  {
+    passwordLength = prompt("Invalid password. Choose password length between (8-128).");
+  }
+}
