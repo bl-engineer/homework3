d4be86c22bdd90fa694592bfb9fdae73562825fb
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-08-25 05:06:39 -0400

    Modifyingjs file to make clean and consize codes

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,23 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the input form
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = prompt("Input password of length (8-128).");
+  while (password.length < 8 || password.length > 128) {
+     password = prompt("Invalide password! The password length must be 8 to 128.");
+  }
+  let generatedPassword = generatePassword(password);
+  let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = generatedPassword;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  // Select password length
-  var passwordLength = prompt("Choose the password length between 8-128.");
-  while (passwordLength < 8 || passwordLength > 128)
-  {
-    passwordLength = prompt("Invalid password. Choose password length between (8-128).");
+
+function generatePassword(oldPassword) {
+  // Generate random characters 
+  let randomCharacters = "";
+  while (randomCharacters == "") {
+    if (confirm("Click OK if you want to include uppercase characters in your password."))
+    {
+      randomCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (confirm("Click OK if you want to include lowercase characters in your password.."))
+    {
+      randomCharacters += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (confirm("Click OK if you want to include numbers in your password.."))
+    {
+      randomCharacters += "0123456789";
+    }
+    if (confirm("Click OK if you want to include special characters in your password.."))
+    {
+      randomCharacters += "!@#$%^&*()_+=-`~<>,./?;:|\\'\"";
+    }
+    if (randomCharacters == "") {
+      alert("You must include at least one random character for the safety of your password.")
+    }
   }
+
+  // Generate secured password with a length extended to double of the input for security
+  let securedPassword = "";
+  for (i = 0; i < oldPassword.length * 2; i++) {
+    securedPassword += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length))
+  }
+  return securedPassword;
 }
