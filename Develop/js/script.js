4663bbb0fec36d1e00b1b15e5b39bb143853ad26
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-08-26 19:31:53 -0400

    Editting both js and README files

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,16 +1,13 @@
-// Assignment Code
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the input form
+// Write password to the #password input
 function writePassword() {
-  var password = prompt("Input password of length (8-128).");
-  while (password.length < 8 || password.length > 128) {
-     password = prompt("Invalide password! The password length must be 8 to 128.");
-  }
-  let generatedPassword = generatePassword(password);
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  // generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  passwordText.value = generatedPassword;
+  passwordText.value = password;
 
 }
 
@@ -18,35 +15,67 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-function generatePassword(oldPassword) {
-  // Generate random characters 
-  let randomCharacters = "";
-  while (randomCharacters == "") {
-    if (confirm("Click OK if you want to include uppercase characters in your password."))
-    {
-      randomCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (confirm("Click OK if you want to include lowercase characters in your password.."))
-    {
-      randomCharacters += "abcdefghijklmnopqrstuvwxyz";
-    }
-    if (confirm("Click OK if you want to include numbers in your password.."))
-    {
-      randomCharacters += "0123456789";
-    }
-    if (confirm("Click OK if you want to include special characters in your password.."))
-    {
-      randomCharacters += "!@#$%^&*()_+=-`~<>,./?;:|\\'\"";
-    }
-    if (randomCharacters == "") {
-      alert("You must include at least one random character for the safety of your password.")
-    }
+function generatePassword() {
+  // Prompt password length 
+  var passwordLength = prompt("Input password length (8-128).");
+  
+  // Check if password length is a type of `number` and not null
+  while (passwordLength.length <= 0 || isNaN(passwordLength)){
+    passwordLength = prompt("Invalide input! The password length must be numbers")
+  }
+  // Check if the password lenght is between 8 to 128 
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Invalide input! The password length must be 8 to 128.");
   }
 
-  // Generate secured password with a length extended to double of the input for security
-  let securedPassword = "";
-  for (i = 0; i < oldPassword.length * 2; i++) {
-    securedPassword += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length))
+  // Select character classes
+  var characterClass = "";
+
+  while (characterClass == "") {
+    // Initialize a counter to track the clicks on `Ok`
+    var countOkClick = 0;
+    if (confirm("Click OK if you want to include uppercase characters in your password."))
+    {
+      // To include uppercase characters
+      characterClass += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      countOkClick += 1;
+    }
+    // To include lowercase characters
+    if (confirm("Click OK if you want to include lowercase characters in your password.."))
+    {
+      characterClass += "abcdefghijklmnopqrstuvwxyz";
+      countOkClick += 1;
+    }
+    // To include numbers
+    if (confirm("Click OK if you want to include numbers in your password.."))
+    {
+      characterClass += "0123456789";
+      countOkClick += 1;
+    }
+    // To include special characters
+    if (confirm("Click OK if you want to include special characters in your password.."))
+    {
+      characterClass += "!@#$%^&*()_+=-`~<>,./?;:|\'";
+      countOkClick += 1;
+    }
+    // To alert and reject if there no any group class included. At least one should be
+    if (characterClass == "") {
+      alert("You must include at least one character class for the safety of your password.")
+    }
+    // Tracking the `Ok` click to alert user and give an option to include more character class for safer passport
+    while (countOkClick == 1){
+      alert("Warning! Your password is too weak. It contains only one type of characters")
+      if (confirm("Click `OK` if you want to include more special characters or click CANCEL to `continue`"))
+        characterClass = ""
+        // Out of the `if`
+        countOkClick += 1;
+    }
   }
-  return securedPassword;
+ 
+  // Generate secured password
+  var password = ""
+  for (i = 0; i < passwordLength; i++) {
+    password += characterClass.charAt(Math.floor(Math.random() * characterClass.length))
+  }
+  return password;
 }
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# homework3
-<!-- Password Generator -->
-This week's Challenge requires you to create an application that an employee can use to generate a random password based on criteria they've selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes. This week's coursework will teach you all the skills you need to succeed in this assignment.
-The password can include special characters.
-
 # user Story
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
@@ -25,8 +20,9 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 
-# Files organization
-I have taken out index.html from `Develop folder`to the `root` so that with `Github pages`when i deploy the site, it won't start with the README but the html
+# Description
+This is application that an employee can use to generate a random password based on criteria they've selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes
+
 
 ---
 © Done by: Bocar Ly
